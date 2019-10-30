2488df2d95034c8c7d2318035c42797eec644f10
Claudia Munaron <claudia.munaron@edu.unito.it>
2019-10-30 11:11:18 +0100

    config

--- icosaf-project/src/api/config.ts
+++ icosaf-project/src/api/config.ts
@@ -4,7 +4,7 @@
 export default {
   dataLogin: 'http://www.mocky.io/v2/5d934c0330000051001b73c1',
   data: 'http://digitwin.cloud.reply.eu/api/2/things/smart%3Adtwin_lwb',
-  insert: 'http:/icowms.cloud.reply.eu/Details/insertRecord',
+  insert: 'http://icowms.cloud.reply.eu/Details/insertRecord',
   details: 'http://icowms.cloud.reply.eu/Details/details',
   amount: 'http://icowms.cloud.reply.eu/Details/selectByDetId?det_short_id='
 };
